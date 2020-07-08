5cf464a11b7186917e2e6d3d8d16ef0d527e3e97
formegusto <formegusto@gmail.com>
2020-07-08 18:59:11 +0900

    Javascript - Functions Study

--- 06_객체/test.js
+++ 06_객체/test.js
@@ -41,3 +41,53 @@ function print_b(hero){
     console.log(text);
 }
 print_b(captainAmerica);
+
+// 객체 비구조화 할당 == 객체 구조 분해 (깔끔버전)
+function print_c({ alias,name,actor }){
+    const text = `${alias}(${name}) 역할 ==> ${actor}`;
+    console.log(text);
+}
+print_c(ironMan)
+
+// 객체 안에 함수 넣기
+const mungmung = {
+    name: '멍멍이',
+    sound: '왜 나는 멍멍하고 울어야 하죠 ㅠ',
+    say: function Say() {
+        console.log(this.sound);
+    }
+};
+mungmung.say();
+    // 주의! 화살표함수는 객체 안에 넣을 수 없다! this가 제대로 자신이 속한 객체를 가리키지 않기 때문!
+
+// Getter 함수와 Setter 함수
+const numbers = {
+    _a:1,
+    _b:2,
+    sum:3,
+    calculate() {
+        console.log('calculate');
+        this.sum = this._a + this._b;
+    },
+    get a() {
+        return this._a;
+    },
+    get b() {
+        return this._b;
+    },
+    set a(value){
+        console.log('a가 췌인지');
+        this._a = value;
+        this.calculate();
+    },
+    set b(value){
+        console.log("b도 췌이지 업");
+        this._b = value;
+        this.calculate();
+    }
+}
+console.log(numbers.sum);
+numbers.a = 5;
+numbers.b = 7;
+numbers.a = 9;
+console.log(numbers.sum);
